63b4364cfed7809f01ad937d87aff0c5e32cfe57
Mbah Shondong <l4nangproduction@gmail.com>
2022-12-07 06:09:07 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install xfce4 xfce4-goodies xfce4-terminal xorg dbus-x11 x11-xserver-utils -y
+sudo apt install firefox -y
+sudo apt install xrdp -y
+sudo adduser xrdp ssl-cert
+sudo systemctl restart xrdp
