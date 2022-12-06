474f318762d562f295ec9134d977a52d38f9ba2b
Mbah Shondong <l4nangproduction@gmail.com>
2022-12-07 06:59:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install xfce4 xfce4-goodies xfce4-terminal xorg dbus-x11 x11-xserver-utils -y
 sudo apt install firefox -y
+sudo apt install chromium-browser -y
 sudo apt install xrdp -y
 sudo adduser xrdp ssl-cert
 sudo systemctl restart xrdp
