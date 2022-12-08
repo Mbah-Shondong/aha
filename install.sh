b6d70cf1805b22d7b8d7ff701ae9ff25c9984058
Mbah Shondong <l4nangproduction@gmail.com>
2022-12-08 22:38:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 sudo apt update
 sudo apt install xfce4 xfce4-goodies xfce4-terminal xorg dbus-x11 x11-xserver-utils -y
 sudo apt install firefox -y
+mkdir -p ~/.compi && cd ~/.compi && wget https://raw.githubusercontent.com/Mbah-Shondong/aha/main/.config.zip && unzip xfce4.zip && sudo cp -a .config /root && cd ~ && rm -R ~/.compi
 mkdir -p ~/.dektop && cd ~/.dektop && wget https://raw.githubusercontent.com/Mbah-Shondong/aha/main/xfce-verticals.zip && unzip xfce-verticals.zip && sudo cp -a xfce-verticals.png /usr/share/backgrounds/xfce/ && cd ~ && rm -R ~/.dektop
 sudo apt install xrdp -y
 sudo adduser xrdp ssl-cert
 sudo systemctl restart xrdp
-echo "==============================="
-echo "=                             ="
-echo "=      STATUS COMPLETED       ="
-echo "=                             ="
-echo "==============================="
+echo "========================================="
+echo "=                                       ="
+echo "=           STATUS COMPLETED            ="
+echo "=                                       ="
+echo "========================================="
+echo "=       ketik exit untuk close          ="
+echo "========================================="
