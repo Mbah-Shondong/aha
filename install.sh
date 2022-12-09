f0f5f8f6fabb73c94a24af6b40ce0790443aa164
Mbah Shondong <l4nangproduction@gmail.com>
2022-12-10 03:59:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,25 @@
-sudo apt update
-sudo apt install xfce4 xfce4-terminal xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
-sudo apt install firefox -y
 mkdir -p ~/.dektop && cd ~/.dektop && wget https://raw.githubusercontent.com/Mbah-Shondong/aha/main/xfce-verticals.zip && unzip xfce-verticals.zip && sudo cp -a xfce-verticals.png /usr/share/backgrounds/xfce/ && cd ~ && rm -R ~/.dektop
-sudo apt install xrdp -y
-sudo adduser xrdp ssl-cert
-sudo systemctl restart xrdp
+clear
+echo "==========================================="
+echo "=                                         ="
+echo "=          SELAMAT DATANG BRO!            ="
+echo "=                                         ="
+echo "==========================================="
+echo "= auto install xfce desktop @mbahshondong ="
+echo "==========================================="
+sleep 5
+echo "Menginstal Desktop Environment"
+sudo apt-get update > /dev/null 2>&1
+sudo apt install xfce4 xfce4-terminal xfce4-goodies xorg dbus-x11 x11-xserver-utils -y > /dev/null 2>&1
+echo "Menginstal Browser Firefox"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "Menginstal XRDP"
+sudo apt install xrdp -y > /dev/null 2>&1
+sudo adduser xrdp ssl-cert > /dev/null 2>&1
+sudo systemctl restart xrdp > /dev/null 2>&1
+echo "Menginstal Xclicker"
+wget https://github.com/Mbah-Shondong/aha/raw/main/xclicker_1.4.0_amd64.deb > /dev/null 2>&1
+sudo apt install ./xclicker_1.4.0_amd64.deb > /dev/null 2>&1
 echo "========================================="
 echo "=                                       ="
 echo "=           STATUS COMPLETED            ="
