8a2fad1796cfe1f2d68ec54c95840222a49e1cb9
Mbah Shondong <l4nangproduction@gmail.com>
2022-12-09 07:43:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install xfce4 xfce4-terminal -y
+sudo apt install xfce4 xfce4-terminal xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
 sudo apt install firefox -y
 mkdir -p ~/.dektop && cd ~/.dektop && wget https://raw.githubusercontent.com/Mbah-Shondong/aha/main/xfce-verticals.zip && unzip xfce-verticals.zip && sudo cp -a xfce-verticals.png /usr/share/backgrounds/xfce/ && cd ~ && rm -R ~/.dektop
 sudo apt install xrdp -y
