a4fcf632c79d7616cb9136f0f3f43d5a73cd7f0d
Mbah Shondong <l4nangproduction@gmail.com>
2022-12-11 06:36:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ echo "==========================================="
 sleep 5
 echo "Menginstal Desktop Environment"
 sudo apt-get update > /dev/null 2>&1
+echo "sabar mas enteni sak wetoro paling 2 menit"
 sudo apt install xfce4 xfce4-terminal xfce4-goodies xorg dbus-x11 x11-xserver-utils -y > /dev/null 2>&1
 echo "Menginstal Browser Firefox"
 sudo apt install firefox -y > /dev/null 2>&1
@@ -21,10 +22,10 @@ wget https://github.com/Mbah-Shondong/aha/raw/main/xclicker_1.4.0_amd64.deb > /d
 sudo apt install ./xclicker_1.4.0_amd64.deb > /dev/null 2>&1
 mkdir -p ~/.dektop && cd ~/.dektop && wget https://raw.githubusercontent.com/Mbah-Shondong/aha/main/xfce-verticals.zip && unzip xfce-verticals.zip && sudo cp -a xfce-verticals.png /usr/share/backgrounds/xfce/ && cd ~ && rm -R ~/.dektop > /dev/null 2>&1
 clear
-echo "========================================="
-echo "=                                       ="
-echo "=           STATUS COMPLETED            ="
-echo "=                                       ="
-echo "========================================="
-echo "=       ketik exit untuk close          ="
-echo "========================================="
+echo "=================================================================================="
+echo "=                                                                                ="
+echo "=                               STATUS COMPLETED                                 ="
+echo "=                                                                                ="
+echo "=================================================================================="
+echo "=                           ketik exit untuk close                               ="
+echo "=================================================================================="
