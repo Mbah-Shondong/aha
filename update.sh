ff6061903cc02cfbdb3568a6b35ecb4d053401f9
Mbah Shondong <l4nangproduction@gmail.com>
2022-12-20 09:41:55 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,26 @@
+
+echo "==========================================="
+echo "=                                         ="
+echo "=          SELAMAT DATANG BRO!            ="
+echo "=                                         ="
+echo "==========================================="
+echo "= auto install xfce desktop @mbahshondong ="
+echo "==========================================="
+sleep 5
+echo "Menginstal Desktop Environment"
+sudo apt-get update
+echo "sabar mas enteni sak wetoro paling 2 menit"
+sudo apt install xfce4 xfce4-terminal -y > /dev/null 2>&1
+echo "Menginstal Browser Firefox"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "Menginstal XRDP"
+sudo apt install xrdp -y > /dev/null 2>&1
+sudo adduser xrdp ssl-cert
+sudo ufw allow 3389
+sudo systemctl restart xrdp
+echo "Menginstal Xclicker"
+wget https://github.com/Mbah-Shondong/aha/raw/main/xclicker_1.4.0_amd64.deb > /dev/null 2>&1
+sudo apt install ./xclicker_1.4.0_amd64.deb > /dev/null 2>&1
 sudo apt update
 sudo apt upgrade -y
 mkdir -p ~/.tema && cd ~/.tema && wget https://github.com/Mbah-Shondong/aha/raw/main/Windows-11.zip && unzip Windows-11.zip && sudo cp -a Windows-11 /usr/share/themes && cd ~ && rm -R ~/.tema
