b938e75f87e374972667da1bc9a616b204ad44ff
Mbah Shondong <l4nangproduction@gmail.com>
2024-01-06 14:25:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,14 @@ echo -e "Mbahe123$\nMbahe123$\n" | sudo passwd
 useradd -m mbahe
 echo 'mbahe:Mbahe123$' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-echo "==========================================="
-echo "=                                         ="
-echo "=          SELAMAT DATANG BRO!            ="
-echo "=                                         ="
-echo "==========================================="
 sudo apt-get update
 sudo apt install xfce4 xfce4-terminal -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install xrdp -y
 sudo adduser xrdp ssl-cert
-sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n xfce4-session \n' /etc/xrdp/startwm.sh
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh
 sudo systemctl restart xrdp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt update
 clear
 echo "=================================================================================="
